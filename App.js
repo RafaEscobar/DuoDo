64be8333b9264c3b09f9ede0c5a742adc1775628
AlexisSM377 <al222111377@gmail.com>
2024-02-01 14:57:48 -0600

    Se agrego fuentes y se configuro tw para poder utilizarlo.

--- App.js
+++ App.js
@@ -1,22 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { useFonts } from "expo-font";
+// import { Inter_900Black } from '@expo-google-fonts/inter';
+import { Comfortaa_700Bold } from "@expo-google-fonts/comfortaa";
+import { Poppins_400Regular } from "@expo-google-fonts/poppins";
 
-import tw from 'twrnc';
+import tw, { style } from "twrnc";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Comfortaa_700Bold,
+    Poppins_400Regular,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={tw`flex-1 justify-center items-center bg-black`}>
-      <Text style={tw`text-white`}>Open up App.js to start working on your app!</Text>
+    <View style={styles.container}>
+      <Text
+        style={{
+          fontFamily: "Comfortaa_700Bold",
+          color: "white",
+          fontSize: 40,
+        }}
+      >
+        Hola Git
+      </Text>
+      <Text style={[styles.text, { fontFamily: "Poppins_400Regular" }]}>
+        Whereas recognition of the inherent dignity
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  container: tw`flex-1 justify-center items-center bg-black`,
+  text: tw`text-yellow-500 text-4xl`,
 });
