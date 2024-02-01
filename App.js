8c3c7e8d3889a59b04977ebcf4ec5bd14953cffd
Rafael Escobar Gutierrez <rafael.eg.code@gmail.com>
2024-02-01 10:43:45 -0600

    Inicializando proyecto

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import tw from 'twrnc';
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={tw`flex-1 justify-center items-center bg-black`}>
+      <Text style={tw`text-white`}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
