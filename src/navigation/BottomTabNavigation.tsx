7a25f8bb5d211a17ef63c13d32a49138d9d3bb1a
AlexisSM377 <al222111377@gmail.com>
2024-02-02 15:54:41 -0600

    Add expo-image package and update BottomTabNavigation icons

--- src/navigation/BottomTabNavigation.tsx
+++ src/navigation/BottomTabNavigation.tsx
@@ -1,16 +1,65 @@
-import React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Calendar, Create, Dashboard, Profile } from '../screens';
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Calendar, Create, Dashboard, Profile } from "../screens";
+import { Image } from "expo-image";
 
 const Tab = createBottomTabNavigator();
 
 export const BottomTabNavigator = () => {
-    return(
-        <Tab.Navigator>
-            <Tab.Screen name='Inicio' component={Dashboard} />
-            <Tab.Screen name='Calendario' component={Calendar} />
-            <Tab.Screen name='Creación' component={Create} />
-            <Tab.Screen name='Info' component={Profile} />
-        </Tab.Navigator>
-    )
-}
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Inicio"
+        component={Dashboard}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Image
+              source="./assets/images/home.png"
+              style={{ tintColor: color, width: 30, height: 30 }}
+            />
+          ),
+          tabBarLabel: () => null,
+        }}
+      />
+      <Tab.Screen
+        name="Calendario"
+        component={Calendar}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Image
+              source="./assets/images/calendario.png"
+              style={{ tintColor: color, width: 30, height: 30 }}
+            />
+          ),
+          tabBarLabel: () => null,
+        }}
+      />
+      <Tab.Screen
+        name="Creación"
+        component={Create}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Image
+              source="./assets/images/tareas.png"
+              style={{ tintColor: color, width: 30, height: 30 }}
+            />
+          ),
+          tabBarLabel: () => null,
+        }}
+      />
+      <Tab.Screen
+        name="Info"
+        component={Profile}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Image
+              source="./assets/images/menu.png"
+              style={{ tintColor: color, width: 30, height: 30 }}
+            />
+          ),
+          tabBarLabel: () => null,
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -1,4 +1,4 @@
 export { Calendar } from "./calendar/Calendar";
-export { Create } from "./create/create";
+export { Create } from "./create/Create";
 export { Dashboard } from "./dashboard/Dashboard";
 export { Profile } from "./profile/Profile";
