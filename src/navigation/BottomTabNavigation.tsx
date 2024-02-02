dba6cbeb89e5a662f5104d995805be2a829dd0bf
Rafael Escobar Gutierrez <rafael.eg.code@gmail.com>
2024-02-02 12:54:21 -0600

    Navegación base generada

--- src/navigation/BottomTabNavigation.tsx
+++ src/navigation/BottomTabNavigation.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Dashboard } from '../screens/dashboard/Dashboard';
 import { Profile } from '../screens/profile/Profile';
+import { Calendar } from '../screens/calendar/Calendar';
+import { Create } from '../screens/create/create';
 
 const Tab = createBottomTabNavigator();
 
 export const BottomTabNavigator = () => {
     return(
         <Tab.Navigator>
-            <Tab.Screen name='Home' component={Dashboard} />
-            <Tab.Screen name='Profile' component={Profile} />
+            <Tab.Screen name='Inicio' component={Dashboard} />
+            <Tab.Screen name='Calendario' component={Calendar} />
+            <Tab.Screen name='Creación' component={Create} />
+            <Tab.Screen name='Info' component={Profile} />
         </Tab.Navigator>
     )
 }
