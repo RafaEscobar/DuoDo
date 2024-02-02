243b8ea0b948be68d99f45c7dd95164544b8d8a1
Rafael Escobar Gutierrez <rafael.eg.code@gmail.com>
2024-02-02 11:49:45 -0600

    Agregando navegación, prueba de funcionamiento

--- App.tsx
+++ App.tsx
@@ -5,9 +5,11 @@ import { Comfortaa_700Bold } from "@expo-google-fonts/comfortaa";
 import { Poppins_400Regular } from "@expo-google-fonts/poppins";
 import { GluestackUIProvider, Box, Spinner, Text } from "@gluestack-ui/themed";
 import { config } from "@gluestack-ui/config";
+import { NavigationContainer } from '@react-navigation/native';
 
 import tw from "twrnc";
 import React from "react";
+import { BottomTabNavigator } from "./src/navigation/BottomTabNavigation";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -20,27 +22,31 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      <Text
-        style={{
-          fontFamily: "Comfortaa_700Bold",
-          color: "white",
-          fontSize: 40,
-        }}
-      >
-        Hola Mundo!
-      </Text>
-      <Text style={[styles.text, { fontFamily: "Poppins_400Regular" }]}>
-        Whereas recognition of the inherent dignity
-      </Text>
-      <GluestackUIProvider config={config}>
-        <Box width="100%" justifyContent="center" borderRadius="$lg" alignItems="center"  bg="$primary500" p="$5" r>
-          <Text style={[styles.text, { fontFamily: "Comfortaa_700Bold" }]}>Open up App.js to start working on your app!</Text>
-        </Box>
-        <Spinner size="small" />
-      </GluestackUIProvider>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <BottomTabNavigator />
+    </NavigationContainer>
+
+    // <View style={styles.container}>
+    //   <Text
+    //     style={{
+    //       fontFamily: "Comfortaa_700Bold",
+    //       color: "white",
+    //       fontSize: 40,
+    //     }}
+    //   >
+    //     Hola Mundo!
+    //   </Text>
+    //   <Text style={[styles.text, { fontFamily: "Poppins_400Regular" }]}>
+    //     Whereas recognition of the inherent dignity
+    //   </Text>
+    //   <GluestackUIProvider config={config}>
+    //     <Box width="100%" justifyContent="center" borderRadius="$lg" alignItems="center"  bg="$primary500" p="$5" r>
+    //       <Text style={[styles.text, { fontFamily: "Comfortaa_700Bold" }]}>Open up App.js to start working on your app!</Text>
+    //     </Box>
+    //     <Spinner size="small" />
+    //   </GluestackUIProvider>
+    //   <StatusBar style="auto" />
+    // </View>
   );
 }
 
--- src/navigation/BottomTabNavigation.tsx
+++ src/navigation/BottomTabNavigation.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Dashboard } from '../screens/dashboard/Dashboard';
+import { Profile } from '../screens/profile.tsx/Profile';
 
 const Tab = createBottomTabNavigator();
 
@@ -8,6 +9,7 @@ export const BottomTabNavigator = () => {
     return(
         <Tab.Navigator>
             <Tab.Screen name='Home' component={Dashboard} />
+            <Tab.Screen name='Profile' component={Profile} />
         </Tab.Navigator>
     )
 }
