7fe60fcc6741e84f0eedb439d8d1d182cc1f239f
Rafael Escobar Gutierrez <rafael.eg.code@gmail.com>
2024-02-20 14:55:41 -0600

    Ajuste

--- src/navigation/StackNavigation.tsx
+++ src/navigation/StackNavigation.tsx
@@ -1,31 +1,17 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Login } from '../screens/login/Login';
 
-import React, { useContext } from 'react';
+import React from 'react';
 import { Landing } from '../screens/login/Landing';
 import { SignUp } from '../screens/login/SignUp';
 import { ResetPassword } from '../screens/login/ResetPassword';
 import { BottomTabNavigator } from './BottomTabNavigation';
-import { AuthContext } from '../context/AuthContext';
-import { Text } from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Stack = createNativeStackNavigator();
 
-export const StackNavigation = async () => {
-
-  let initialScreen;
-  const token = await AsyncStorage.getItem('u-token');
-  if (token === null) {
-
-    initialScreen = 'Login';
-  } else {
-
-    initialScreen = 'BottomTabNavigator';
-  }
-
+export const StackNavigation = () => {
   return (
-    <Stack.Navigator initialRouteName='initialScreen'>
+    <Stack.Navigator >
       <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
       <Stack.Screen name="BottomTabNavigator" component={BottomTabNavigator} options={{ headerShown: false }} />
       <Stack.Screen name="Landing" component={Landing} options={{ headerShown: false }} />
