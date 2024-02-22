3ff5145eb7560f20396a751dea0eedb63910bcc6
Rafael Escobar Gutierrez <rafael.eg.code@gmail.com>
2024-02-22 09:42:38 -0600

    Corrección

--- src/modules/api/LoginModule.tsx
+++ src/modules/api/LoginModule.tsx
@@ -1,7 +1,7 @@
 import { BASE_URL_AUTH } from '@env';
 
 export const LoginModule = async ({ email, password }:any) => {
-    const url = `https://7741-2806-2f0-9f00-ffaf-3d98-5033-4c39-b426.ngrok-free.app/api/login`;
+    const url = `${BASE_URL_AUTH}/login`;
     const response = await fetch(url, {
         method: 'POST',
         headers: {
