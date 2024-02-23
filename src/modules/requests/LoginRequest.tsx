585d2855aeb61c5f35e6bb068fdaf049f138a7d8
Rafael Escobar Gutierrez <rafael.eg.code@gmail.com>
2024-02-23 12:10:58 -0600

    Documentación y tipado de LoginRequest en pausa

--- src/modules/requests/LoginRequest.tsx
+++ src/modules/requests/LoginRequest.tsx
@@ -1,4 +1,17 @@
-export const LoginRequest = async ({ email, password }:any) => {
+/**
+ ** Typed for login function parameters
+ */
+interface LoginParams {
+  email: string,
+  password: string
+}
+
+/**
+ ** Function to login request
+ * @param email_password - User credentials
+ * @returns void
+ */
+export const LoginRequest = async ({ email, password }:LoginParams) => {
     const url = `https://c533-187-235-143-15.ngrok-free.app/api/login`;
     const response = await fetch(url, {
         method: 'POST',
