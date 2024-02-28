fcf9e493e3f3237cf673a8b644ecf2eba86079dd
Rafael Escobar Gutierrez <rafael.eg.code@gmail.com>
2024-02-27 18:32:35 -0600

    Previo a recivir avatar

--- App.tsx
+++ App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { AuthProvider } from './src/context/AuthContext';
 import { CustomNavigation } from './src/navigation/CustomNavigation';
-import { ValidatingLogin } from './src/screens/auth/ValidatingLogin';
 
 export default function App(){
   console.log(process.env.AUTH_URL);
