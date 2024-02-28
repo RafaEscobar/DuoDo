794b064100358b52fd0e0f3178b3806f4c4730dc
Rafael Escobar Gutierrez <rafael.eg.code@gmail.com>
2024-02-28 11:01:03 -0600

    Ajuste final

--- src/screens/create/Create.tsx
+++ src/screens/create/Create.tsx
@@ -1,10 +1,9 @@
 import { View, Text } from 'react-native';
 import React from 'react';
-import tw from 'twrnc';
 
 export const Create = () => {
     return (
-        <View style={tw`flex justify-center items-center h-full bg-[#271C3A]`}>
+        <View>
             <Text>Create</Text>
         </View>
     )
