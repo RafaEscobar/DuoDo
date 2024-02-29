91496c22dcd2bcaed1c4b0e0ae2fa9498ccbb20e
AlexisSM377 <al222111377@gmail.com>
2024-02-29 08:19:21 -0600

    se ajusto la ruta que habia puesto como ejempl0

--- App.tsx
+++ App.tsx
@@ -1,18 +1,12 @@
 import React from 'react';
 import { AuthProvider } from './src/context/AuthContext';
 import { CustomNavigation } from './src/navigation/CustomNavigation';
-import { ValidatingLogin } from './src/screens/auth/ValidatingLogin';
-import { DashboardStack } from './src/navigation/DashboardStack';
-import { BottomTabNavigation } from './src/navigation/BottomTabNavigation';
-import { AddTask } from './src/screens/create/AddTask';
-import { AllTask } from './src/screens/create/AllTask';
 
-export default function App(){
-  // console.log(process.env.AUTH_URL);
+export default function App() {
+  console.log(process.env.AUTH_URL);
   return (
     <AuthProvider>
-      {/* <AllTask /> */}
-      <AddTask />
+      <CustomNavigation />
     </AuthProvider>
   );
 }
