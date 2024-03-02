28b7a543ede682f250e48ed97ad4092cfecb96dc
Rafael Escobar Gutierrez <rafael.eg.code@gmail.com>
2024-03-02 17:48:35 -0600

    Generando funcionalidad de modal

--- App.tsx
+++ App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { AuthProvider } from './src/context/AuthContext';
 import { CustomNavigation } from './src/navigation/CustomNavigation';
-import { SelectAvatar } from './src/screens/auth/SelectAvatar';
 import { ActionSheetProvider } from '@expo/react-native-action-sheet';
 
 export default function App(){
--- src/screens/calendar/Calendar.tsx
+++ src/screens/calendar/Calendar.tsx
@@ -1,42 +1,14 @@
+import { BottonSheetComponent } from '../../component/BottonSheetComponent';
 import { useActionSheet } from '@expo/react-native-action-sheet';
-import React from 'react';
 import { View, Text, Button } from 'react-native';
+import React from 'react';
 
 export const Calendar = () => {
     const { showActionSheetWithOptions } = useActionSheet();
-
-    const showBottomSheet = () => {
-        const options = ['Tarea', 'Espacio de trabajo', 'Cerrar'];
-
-        const cancelButtonIndex = 2;
-        const title = '¿Qué deseas crear?';
-        const message = 'Puedes generar una tarea individual/colaborativa o un espacio de trabajo para mantener tus pendientes más organizados y llevar un seguimiento de los mismos.';
-
-        showActionSheetWithOptions({
-            options,
-            cancelButtonIndex,
-            title,
-            message,
-            cancelButtonTintColor:'red'
-        },
-        (selectedIndex) => {
-            switch (selectedIndex) {
-                case 1:
-                // Save
-                break;
-
-                case 2:
-                // Delete
-                break;
-                case cancelButtonIndex:
-                // Canceled
-            }
-        });
-    }
     return (
         <View >
             <Text>Nose</Text>
-            <Button title="Menu" onPress={showBottomSheet}/>
+            <Button title="Menu" onPress={() => {BottonSheetComponent(showActionSheetWithOptions)}}/>
         </View>
     )
 }
