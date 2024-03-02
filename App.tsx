d6470a0849c5b47be0cd0283bfd2c4055475de2d
Rafael Escobar Gutierrez <rafael.eg.code@gmail.com>
2024-03-02 17:41:24 -0600

    Botton sheet generado

--- App.tsx
+++ App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { AuthProvider } from './src/context/AuthContext';
 import { CustomNavigation } from './src/navigation/CustomNavigation';
 import { SelectAvatar } from './src/screens/auth/SelectAvatar';
+import { ActionSheetProvider } from '@expo/react-native-action-sheet';
 
 export default function App(){
   return (
-    <AuthProvider>
-      <CustomNavigation />
-      {/* <SelectAvatar /> */}
-    </AuthProvider>
+    <ActionSheetProvider>
+      <AuthProvider>
+        <CustomNavigation />
+      </AuthProvider>
+    </ActionSheetProvider>
   );
 }
--- src/screens/calendar/Calendar.tsx
+++ src/screens/calendar/Calendar.tsx
@@ -1,10 +1,42 @@
+import { useActionSheet } from '@expo/react-native-action-sheet';
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 
 export const Calendar = () => {
+    const { showActionSheetWithOptions } = useActionSheet();
+
+    const showBottomSheet = () => {
+        const options = ['Tarea', 'Espacio de trabajo', 'Cerrar'];
+
+        const cancelButtonIndex = 2;
+        const title = '¿Qué deseas crear?';
+        const message = 'Puedes generar una tarea individual/colaborativa o un espacio de trabajo para mantener tus pendientes más organizados y llevar un seguimiento de los mismos.';
+
+        showActionSheetWithOptions({
+            options,
+            cancelButtonIndex,
+            title,
+            message,
+            cancelButtonTintColor:'red'
+        },
+        (selectedIndex) => {
+            switch (selectedIndex) {
+                case 1:
+                // Save
+                break;
+
+                case 2:
+                // Delete
+                break;
+                case cancelButtonIndex:
+                // Canceled
+            }
+        });
+    }
     return (
         <View >
             <Text>Nose</Text>
+            <Button title="Menu" onPress={showBottomSheet}/>
         </View>
     )
 }
