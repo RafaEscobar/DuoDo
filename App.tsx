0f2db89587c884b8c83aa435c6e0865f1a9b312e
Rafael Escobar Gutierrez <rafael.eg.code@gmail.com>
2024-03-14 10:10:02 -0600

    Construcción de build con eas

--- App.tsx
+++ App.tsx
@@ -1,8 +1,11 @@
 import { AuthProvider } from './src/context/AuthContext';
+import { useNotification } from './src/hooks/useNotification';
 import { CustomNavigation } from './src/navigation/CustomNavigation';
 import React from 'react';
 
 export default function App(){
+  const expoPushToken = useNotification();
+  console.log(expoPushToken);
   return (
       <AuthProvider>
         <CustomNavigation />
---
+++ src/hooks/useNotification.tsx
@@ -0,0 +1,57 @@
+import { useState, useEffect, useRef } from 'react';
+import { Platform } from 'react-native';
+import * as Device from 'expo-device';
+import * as Notifications from 'expo-notifications';
+import Constants from 'expo-constants';
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
+
+async function registerForPushNotificationsAsync() {
+    let token;
+
+    if (Platform.OS === 'android') {
+    Notifications.setNotificationChannelAsync('default', {
+        name: 'default',
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: '#FF231F7C',
+    });
+    }
+
+    if (Device.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+        alert('Failed to get push token for push notification!');
+        return;
+    }
+    token = await Notifications.getExpoPushTokenAsync({
+        projectId: Constants.expoConfig.extra.eas.projectId,
+    });
+        console.log(token);
+    } else {
+        alert('Must use physical device for Push Notifications');
+    }
+
+    return token.data;
+}
+
+export const useNotification = () => {
+    const [expoPushToken, setExpoPushToken] = useState('');
+
+    useEffect(() => {
+        registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
+    }, []);
+
+    return expoPushToken;
+}
