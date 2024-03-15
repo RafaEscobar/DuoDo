f2f05241ebc18030d905316031589db50ffd1c74
Rafael Escobar Gutierrez <rafael.eg.code@gmail.com>
2024-03-15 09:56:04 -0600

    Quitando logs

--- src/modules/requests/RegisterRequest.tsx
+++ src/modules/requests/RegisterRequest.tsx
@@ -3,11 +3,7 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 export const RegisterRequest = async (name:any, last_name:any, email:any, password:any, birthdate:any, authUrl:any) => {
     try {
         const expoPushToken = await AsyncStorage.getItem('expo-push-token');
-        console.log(expoPushToken);
-        console.log(typeof expoPushToken);
-        //  const url = `${authUrl}/register`;
-        const url = `https://e04c-187-235-154-185.ngrok-free.app/api/register`;
-        console.log(url);
+        const url = `${authUrl}/register`;
         const params = {
             name,
             last_name,
