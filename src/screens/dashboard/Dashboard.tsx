f919fcfcc55c8e051c7c7cdf771d09d8e884973f
Rafael Escobar Gutierrez <rafael.eg.code@gmail.com>
2024-04-02 10:38:19 -0600

    Implementando alertas

--- src/screens/dashboard/Dashboard.tsx
+++ src/screens/dashboard/Dashboard.tsx
@@ -1,15 +1,14 @@
 import { AuthContext } from '../../context/AuthContext';
 import { Image } from "expo-image";
 import { Ionicons } from '@expo/vector-icons';
-import { MaterialIcons } from '@expo/vector-icons';
-import { Octicons } from '@expo/vector-icons';
 import { Poppins_400Regular, Poppins_700Bold } from "@expo-google-fonts/poppins";
 import { SwiperComponent } from '../../component/SwiperComponent';
 import { TaskComponent } from '../../component/TaskComponent';
 import { useFonts } from 'expo-font';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, Button } from 'react-native';
 import React, { useContext } from 'react';
 import tw from 'twrnc';
+import { ALERT_TYPE, Dialog, AlertNotificationRoot, Toast } from 'react-native-alert-notification';
 
 export const Dashboard = ({ navigation: { navigate } }: any) => {
     const { user }: any = useContext(AuthContext);
@@ -27,46 +26,58 @@ export const Dashboard = ({ navigation: { navigate } }: any) => {
     }
 
     return (
-        <View style={tw`bg-[#271C3A]`}>
-            <View style={tw`flex ml-3 mt-10 sm:ml-4 sm:mt-5`}>
+        <AlertNotificationRoot>
+            <View style={tw`bg-[#271C3A]`}>
+                <View style={tw`flex ml-3 mt-10 sm:ml-4 sm:mt-5`}>
+                    <View>
+                        <View>
+                            <Text style={[tw`text-3xl text-white`, { fontFamily: "Poppins_700Bold" }]}>{currentUser.name}</Text>
+                            <Text style={[tw`text-lg opacity-80 text-indigo-200`, { fontFamily: "Poppins_400Regular" }]}>Desarrollador Web</Text>
+                        </View>
+                    </View>
+                    <View style={tw`flex flex-row-reverse items-center -top-15 -left-5 gap-2`}>
+                        <Ionicons name="notifications" size={25} color="black" style={tw`bg-orange-400 p-2 rounded-2xl mb-4`} />
+                        <Image
+                            style={tw`w-15 h-15 rounded-full`}
+                            source={urlImage}
+                        />
+                    </View>
+                </View>
+                <Button
+                        title={'toast notification'}
+                        onPress={() =>
+                            Toast.show({
+                            type: ALERT_TYPE.WARNING,
+                            title: 'Success',
+                            textBody: 'Congrats! this is toast notification success',
+                            })
+                        }
+                    />
                 <View>
                     <View>
-                        <Text style={[tw`text-3xl text-white`, { fontFamily: "Poppins_700Bold" }]}>{currentUser.name}</Text>
-                        <Text style={[tw`text-lg opacity-80 text-indigo-200`, { fontFamily: "Poppins_400Regular" }]}>Desarrollador Web</Text>
+                        <View style={tw`flex flex-row justify-between items-center w-90 ml-3`}>
+                            <Text style={[tw`text-3xl text-white`, { fontFamily: "Poppins_700Bold" }]}>Workspace</Text>
+                            <TouchableOpacity onPress={() => navigate('Workspace')}>
+                                <Text style={[tw`text-base text-sky-500`, { fontFamily: "Poppins_700Bold" }]}>Ver todas</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                    <SwiperComponent />
+                </View>
+                <View style={tw`flex ml-3 mt-6 sm:ml-4 sm:mt-5`}>
+                    <View>
+                        <View style={tw`flex flex-row justify-between items-center w-90`}>
+                            <Text style={[tw`text-3xl text-white`, { fontFamily: "Poppins_700Bold" }]}>Tareas</Text>
+                            <TouchableOpacity onPress={() => navigate('Task')}>
+                                <Text style={[tw`text-base text-sky-500`, { fontFamily: "Poppins_700Bold" }]}>Ver todas</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                    <View style={tw`w-92 h-85`}>
+                        <TaskComponent />
                     </View>
                 </View>
-                <View style={tw`flex flex-row-reverse items-center -top-15 -left-5 gap-2`}>
-                    <Ionicons name="notifications" size={25} color="black" style={tw`bg-orange-400 p-2 rounded-2xl mb-4`} />
-                    <Image
-                        style={tw`w-15 h-15 rounded-full`}
-                        source={urlImage}
-                    />
-                </View>
             </View>
-            <View>
-                <View>
-                    <View style={tw`flex flex-row justify-between items-center w-90 ml-3`}>
-                        <Text style={[tw`text-3xl text-white`, { fontFamily: "Poppins_700Bold" }]}>Workspace</Text>
-                        <TouchableOpacity onPress={() => navigate('Workspace')}>
-                            <Text style={[tw`text-base text-sky-500`, { fontFamily: "Poppins_700Bold" }]}>Ver todas</Text>
-                        </TouchableOpacity>
-                    </View>
-                </View>
-                <SwiperComponent />
-            </View>
-            <View style={tw`flex ml-3 mt-6 sm:ml-4 sm:mt-5`}>
-                <View>
-                    <View style={tw`flex flex-row justify-between items-center w-90`}>
-                        <Text style={[tw`text-3xl text-white`, { fontFamily: "Poppins_700Bold" }]}>Tareas</Text>
-                        <TouchableOpacity onPress={() => navigate('Task')}>
-                            <Text style={[tw`text-base text-sky-500`, { fontFamily: "Poppins_700Bold" }]}>Ver todas</Text>
-                        </TouchableOpacity>
-                    </View>
-                </View>
-                <View style={tw`w-92 h-85`}>
-                    <TaskComponent />
-                </View>
-            </View>
-        </View>
+        </AlertNotificationRoot>
     )
 }
