78d64eac18dbc2314b7957806941e57f427ed882
AlexisSM377 <al222111377@gmail.com>
2024-04-04 11:00:42 -0600

    Add @types/react-native-calendars package

--- src/screens/calendar/Calendar.tsx
+++ src/screens/calendar/Calendar.tsx
@@ -1,11 +1,49 @@
-import { View, Text } from 'react-native';
-import React from 'react';
-import tw from 'twrnc';
+import React, { useState } from 'react';
+import { Text, View } from 'react-native';
+import { Agenda } from 'react-native-calendars';
 
-export const Calendar = ({ navigate }: any) => {
+export const Calendar = () => {
+    const [items, setItems] = useState({});
+
+    const loadItems = (day) => {
+        setTimeout(() => {
+            for (let i = -15; i < 85; i++) {
+                const time = day.timestamp + i * 24 * 60 * 60 * 1000;
+                const strTime = new Date(time).toISOString().split('T')[0];
+                if (!items[strTime]) {
+                    items[strTime] = [];
+                    const numItems = Math.floor(Math.random() * 5);
+                    for (let j = 0; j < numItems; j++) {
+                        items[strTime].push({
+                            name: 'Item for ' + strTime + ' #' + j,
+                            height: Math.max(50, Math.floor(Math.random() * 150))
+                        });
+                    }
+                }
+            }
+            const newItems = {};
+            Object.keys(items).forEach(key => { newItems[key] = items[key]; });
+            setItems(newItems);
+        }, 1000);
+    };
+
+    const renderItem = (item: any) => {
+        return (
+            <View>
+                <Text>{item.name}</Text>
+            </View>
+        );
+    }
+
     return (
-        <View style={tw`flex pt-10 ml-2.5`}>
-            <Text style={tw`text-4xl text-center`}>Aqui va ir el calendario</Text>
+        <View style={{ flex: 1, paddingTop: 30 }}>
+            <Agenda
+                items={items}
+                loadItemsForMonth={loadItems}
+                selected={'2022-05-16'}
+                renderItem={renderItem}
+            />
         </View>
-    )
-}
+    );
+};
+
